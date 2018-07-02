1ca10784b04421cd29f74e306055fffee65fa34c
Sam Sanders <sam-sanders@hotmail.co.uk>
2018-07-02 14:23:32 +1200

    add wombat

--- index.js
+++ index.js
@@ -38,19 +38,33 @@ function render () {
   const state = store.getState()
   const wombats = state.wombats
   document.getElementById('app').innerHTML = renderWombats(wombats)
-  document.getElementById('wombat').addEventListener('click', () => {
-    store.dispatch({
-      type: 'DEL_WOMBAT',
-      wombat: document.getElementById('wombat').name
-    })
-  })
+  document.getElementById('wombat').addEventListener('click', deleteWombat)
+  document.getElementById('but').addEventListener('click', addWombat)
 }
 
 function renderWombats (wombats) {
+  let add = `<input type='text'id='add'> <input id='but' type='submit' value='Add Wombat'>`
+  let edit = `<input type='text'id='edit'> <input id='butty' type='submit' value='Edit Wombat'>`
   let output = '<ul>'
   for (const wombat of wombats) {
     output += `<li>${wombat} <button id='wombat' name=${wombat}>Delete</li><br>`
   }
   output += '</ul>'
-  return output
+  return output + add
 }
+
+function addWombat () {
+  store.dispatch({
+    type: 'ADD_WOMBAT',
+    wombat: document.getElementById('add').value
+  })
+}
+
+function deleteWombat () {
+  store.dispatch({
+    type: 'DEL_WOMBAT',
+    wombat: document.getElementById('wombat').name
+  })
+}
+
+function updateWombat
